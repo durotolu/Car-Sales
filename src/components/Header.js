39cd11e7d9d5f933da7e302240daa591068e6e06
Modurotolu Olokode <durolawk@gmail.com>
2019-10-09 17:59:47 +0100

    added Total functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,19 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../state/actionCreators'
 
 const Header = props => {
+  debugger
   return (
     <>
       <figure className="image is-128x128">
         <img src={props.state.car.image} alt={props.state.car.name} />
       </figure>
       <h2>{props.state.car.name}</h2>
-      <p>Amount: ${props.state.car.price}</p>
+      <p>{ 
+        props.state.car.features.length ?
+        props.state.car.features.reduce((acc, feature) => acc + feature.price, props.state.car.price):
+        props.state.car.price
+      }
+      </p>
     </>
   );
 };
